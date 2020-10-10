bd1ee23a32bdcdd3faee4d2a20832279af1ed290
Shadhin73 <67996141+Shadhin73@users.noreply.github.com>
2020-10-10 22:28:28 +0600

    Create tut1.py

---
+++ tut1.py
@@ -0,0 +1,32 @@
+#Please dont remove this line
+"""
+This is a
+Multiline Comment
+"""
+"""
+This is a comment
+"""
+# print("Subscribe CodeWithHarry now","Bhai video bhi like kar dena")
+# print("next line")
+# print("C:\'narry")
+print("Harry is \n good boy \t1") #comment after statement
+
+
+
+
+
+"""
+end: end argument allows us to put something at the end of the line after it is printed. In simple words, it allows us to continue the line with " " or ',' or anything we put inside these quotes of the end.
+It simply joins two different print statements using some string or even by space. Example:
+
+"""
+import os
+import flask
+
+# print statement for printing strings
+print("Harry is a programmer", end="**")
+
+# Print statement with a literal
+print(1+87)
+
+#This will print "Harry is a programmer**88" on the screen 
