71f23b49cae1c763052b9c6d7129c12999658fb9
Shahrear Abedin <67996141+ShahrearAbedin@users.noreply.github.com>
2020-10-27 10:52:38 +0600

    Update 20_guessing_num.py

--- 20_guessing_num.py
+++ 20_guessing_num.py
@@ -1,16 +1,17 @@
-print("You will get 5 chances")
+import random
+print("You will get 5 chances to guess a number between 0 to 10")
 num_guess = 5
+num = random.randint(0, 10)
 while num_guess > 0:
-    num = 5
     num_guess = num_guess - 1
     yr_guess = int(input("Guess the hidden number: "))
-    if num < num_guess:
-        print("Your number is high and you remaining guess", num_guess)
-    elif num > num_guess:
-        print("Your number is low and you remaining guess", num_guess)
-    elif num == yr_guess:
+    if num == yr_guess:
         print("Success you guessed the correct number")
         break
+    else:
+        if num > yr_guess:
+            print("Your number is low and you remaining guess", num_guess)
+        else:
+            print("Your number is high and you remaining guess", num_guess)
     if num_guess == 0:
         print("The number was", num)
-
