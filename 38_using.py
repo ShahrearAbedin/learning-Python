f90a1d1bff7b3267f30d185948c3fb24a816cf61
Shahrear Abedin <67996141+ShahrearAbedin@users.noreply.github.com>
2020-11-06 11:23:56 +0600

    Add files via upload

---
+++ 38_using.py
@@ -0,0 +1,9 @@
+import random
+import math
+random_number = random.randint(0, 10)
+# print(random_number)
+rand = math.ceil(random.random() * 10)
+# print(rand)
+lst = ["Disney", "Star-Plus", "Hungama", "Aah-taak", "ATN", "BBC"]
+choice = random.choice(lst)
+# print(choice)
---
+++ 39_f-strings.py
@@ -0,0 +1,11 @@
+import math
+# F strings
+me = "Shadhin"
+you = "Shahrear"
+# a = "This is %s %s"%(me, you)
+
+# a = "This is {1} {0}"
+# b = a.format(me, you)
+
+a = f"this is {me} {you} {math.sin(90)}"
+print(a)
